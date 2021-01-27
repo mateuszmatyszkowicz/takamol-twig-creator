0c9fc1d0f78cffa90500d826693954db546df063
Adam Pałka <adam.palka@boldare.com>
2021-01-27 21:07:48 +0100

    Add weekly-report pdf

--- server.js
+++ server.js
@@ -46,6 +46,193 @@ app.get("/invoice", (req, res) => {
   });
 });
 
+app.get("/weekly-report", (req, res) => {
+  res.render("weekly-report.twig", {
+    'weeklyReport': [
+      {
+        'icIdentity': '1-2345',
+        'icName': 'Bank of America',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '4-578',
+        'icName': 'Bank of America2',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-246345',
+        'icName': 'Bank of America3',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-232332',
+        'icName': 'Bank of America4',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-3333',
+        'icName': 'Bank of America5',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-123321',
+        'icName': 'Bank of America6',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1111',
+        'icName': 'Bank of America7',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-321',
+        'icName': 'Bank of America8',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-898',
+        'icName': 'Bank of America9',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1010',
+        'icName': 'Bank of America10',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1111',
+        'icName': 'Bank of America11',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1112',
+        'icName': 'Bank of America13',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1112',
+        'icName': 'Bank of America14',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1112',
+        'icName': 'Bank of America15',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1112',
+        'icName': 'Bank of America16',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1112',
+        'icName': 'Bank of America17',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1112',
+        'icName': 'Bank of America18',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1112',
+        'icName': 'Bank of America19',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      },
+      {
+        'icIdentity': '2-1112',
+        'icName': 'Bank of America20',
+        'numberOfLaborers': 1267,
+        'leasedLaborers': 973,
+        'unleasedLaborers': 294,
+        'weeklyIndicator': 83,
+        'monthlyIndicator': 76
+      }
+    ]
+  });
+});
+
+app.get("/weekly-report-header", (req, res) => {
+  res.render("weekly-report-header.twig", {});
+});
+
+app.get("/weekly-report-footer", (req, res) => {
+  res.render("weekly-report-footer.twig", {});
+});
+
+
 app.get("/notice", (req, res) => {
   res.render("notice.twig", {});
 });
