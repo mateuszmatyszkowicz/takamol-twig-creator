b8207706870491c37c2b6148ee42b23b41341a82
Adam Pałka <adam.palka@boldare.com>
2023-02-22 13:59:16 +0100

    imp: update styles for visa weekly report

--- data/visa-weekly-report.js
+++ data/visa-weekly-report.js
@@ -1,311 +1,351 @@
 module.exports = {
   headerData: {
     currentDate: "19-02-2023",
-  },
-  data: {
     launchDate: "2022-01-01",
     weekStartDate: "2023-02-19",
+  },
+  data: {
     totalIssued: 1234,
-    list: [
+    weeklyReportData: [
       {
         establishmentName: "فرع شركة الفلك للخدمات العمالية مساهمة مقفلة",
-        issued: {
-          sinceLaunch: 407,
-          lastWeek: 35,
-        },
-        cancelledAjeer: {
-          sinceLaunch: 44,
-          lastWeek: 8,
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 44,
-          lastWeek: 8,
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 407,
+            lastWeek: 35,
+          },
+          canceledAjeer: {
+            sinceLaunch: 44,
+            lastWeek: 8,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 44,
+            lastWeek: 8,
+          },
         },
       },
       {
         establishmentName: "شركة الصقر العربي للتجارة",
-        issued: {
-          sinceLaunch: 430,
-          lastWeek: 36,
-        },
-        cancelledAjeer: {
-          sinceLaunch: 40,
-          lastWeek: 7,
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 18,
-          lastWeek: 4,
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 430,
+            lastWeek: 36,
+          },
+          canceledAjeer: {
+            sinceLaunch: 40,
+            lastWeek: 7,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 18,
+            lastWeek: 4,
+          },
         },
       },
       {
         establishmentName: "شركة الأحمدية للخدمات اللوجستية",
-        issued: {
-          sinceLaunch: 320,
-          lastWeek: 28,
-        },
-        cancelledAjeer: {
-          sinceLaunch: 35,
-          lastWeek: 10,
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 22,
-          lastWeek: 3,
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 320,
+            lastWeek: 28,
+          },
+          canceledAjeer: {
+            sinceLaunch: 35,
+            lastWeek: 10,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 22,
+            lastWeek: 3,
+          },
         },
       },
       {
         establishmentName: "شركة النور الحرفية",
-        issued: {
-          sinceLaunch: 250,
-          lastWeek: 22,
-        },
-        cancelledAjeer: {
-          sinceLaunch: 20,
-          lastWeek: 5,
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 12,
-          lastWeek: 2,
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 250,
+            lastWeek: 22,
+          },
+          canceledAjeer: {
+            sinceLaunch: 20,
+            lastWeek: 5,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 12,
+            lastWeek: 2,
+          },
         },
       },
       {
         establishmentName: "شركة المحيط الأزرق للتجارة والتسويق",
-        issued: {
-          sinceLaunch: 150,
-          lastWeek: 12,
-        },
-        cancelledAjeer: {
-          sinceLaunch: 16,
-          lastWeek: 3,
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 10,
-          lastWeek: 1,
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 150,
+            lastWeek: 12,
+          },
+          canceledAjeer: {
+            sinceLaunch: 16,
+            lastWeek: 3,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 10,
+            lastWeek: 1,
+          },
         },
       },
       {
         establishmentName: "شركة النورس الذهبي للتجارة",
-        issued: {
-          sinceLaunch: 290,
-          lastWeek: 24,
-        },
-        cancelledAjeer: {
-          sinceLaunch: 28,
-          lastWeek: 9,
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 15,
-          lastWeek: 3,
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 290,
+            lastWeek: 24,
+          },
+          canceledAjeer: {
+            sinceLaunch: 28,
+            lastWeek: 9,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 15,
+            lastWeek: 3,
+          },
         },
       },
       {
         establishmentName: "شركة الفيصلية للصناعات الغذائية",
-        issued: {
-          sinceLaunch: 380,
-          lastWeek: 34,
-        },
-        cancelledAjeer: {
-          sinceLaunch: 42,
-          lastWeek: 6,
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 20,
-          lastWeek: 4,
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 380,
+            lastWeek: 34,
+          },
+          canceledAjeer: {
+            sinceLaunch: 42,
+            lastWeek: 6,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 20,
+            lastWeek: 4,
+          },
         },
       },
       {
         establishmentName: "شركة الأعمال الرائدة للتجارة والتسويق",
-        issued: {
-          sinceLaunch: 520,
-          lastWeek: 42
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 520,
+            lastWeek: 42,
+          },
+          canceledAjeer: {
+            sinceLaunch: 58,
+            lastWeek: 9,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 27,
+            lastWeek: 5,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 58,
-          lastWeek: 9
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 27,
-          lastWeek: 5
-        }
       },
       {
         establishmentName: "شركة المعالم الجديدة للتجارة والتسويق",
-        issued: {
-          sinceLaunch: 400,
-          lastWeek: 32
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 400,
+            lastWeek: 32,
+          },
+          canceledAjeer: {
+            sinceLaunch: 47,
+            lastWeek: 8,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 21,
+            lastWeek: 4,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 47,
-          lastWeek: 8
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 21,
-          lastWeek: 4
-        }
       },
       {
         establishmentName: "شركة الأهداف المشرقة للخدمات اللوجستية",
-        issued: {
-          sinceLaunch: 320,
-          lastWeek: 25
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 320,
+            lastWeek: 25,
+          },
+          canceledAjeer: {
+            sinceLaunch: 33,
+            lastWeek: 6,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 16,
+            lastWeek: 3,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 33,
-          lastWeek: 6
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 16,
-          lastWeek: 3
-        }
       },
       {
         establishmentName: "شركة المستقبل الجديد للخدمات اللوجستية",
-        issued: {
-          sinceLaunch: 250,
-          lastWeek: 20
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 250,
+            lastWeek: 20,
+          },
+          canceledAjeer: {
+            sinceLaunch: 25,
+            lastWeek: 5,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 12,
+            lastWeek: 2,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 25,
-          lastWeek: 5
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 12,
-          lastWeek: 2
-        }
       },
       {
         establishmentName: "شركة الريادة العالمية للتجارة والتسويق",
-        issued: {
-          sinceLaunch: 360,
-          lastWeek: 29
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 360,
+            lastWeek: 29,
+          },
+          canceledAjeer: {
+            sinceLaunch: 40,
+            lastWeek: 7,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 19,
+            lastWeek: 4,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 40,
-          lastWeek: 7
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 19,
-          lastWeek: 4
-        }
       },
       {
         establishmentName: "شركة الواحة النشطة للخدمات العمالية",
-        issued: {
-          sinceLaunch: 280,
-          lastWeek: 23
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 280,
+            lastWeek: 23,
+          },
+          canceledAjeer: {
+            sinceLaunch: 32,
+            lastWeek: 6,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 15,
+            lastWeek: 3,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 32,
-          lastWeek: 6
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 15,
-          lastWeek: 3
-        }
       },
       {
         establishmentName: "شركة الأفق الجديد للتجارة والتسويق",
-        issued: {
-          sinceLaunch: 470,
-          lastWeek: 38
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 470,
+            lastWeek: 38,
+          },
+          canceledAjeer: {
+            sinceLaunch: 52,
+            lastWeek: 10,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 23,
+            lastWeek: 5,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 52,
-          lastWeek: 10
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 23,
-          lastWeek: 5
-        }
       },
       {
         establishmentName: "شركة المعالم الجديدة للتجارة والتسويق",
-        issued: {
-          sinceLaunch: 400,
-          lastWeek: 32
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 400,
+            lastWeek: 32,
+          },
+          canceledAjeer: {
+            sinceLaunch: 47,
+            lastWeek: 8,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 21,
+            lastWeek: 4,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 47,
-          lastWeek: 8
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 21,
-          lastWeek: 4
-        }
       },
       {
         establishmentName: "شركة الأهداف المشرقة للخدمات اللوجستية",
-        issued: {
-          sinceLaunch: 320,
-          lastWeek: 25
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 320,
+            lastWeek: 25,
+          },
+          canceledAjeer: {
+            sinceLaunch: 33,
+            lastWeek: 6,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 16,
+            lastWeek: 3,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 33,
-          lastWeek: 6
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 16,
-          lastWeek: 3
-        }
       },
       {
         establishmentName: "شركة المستقبل الجديد للخدمات اللوجستية",
-        issued: {
-          sinceLaunch: 250,
-          lastWeek: 20
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 250,
+            lastWeek: 20,
+          },
+          canceledAjeer: {
+            sinceLaunch: 25,
+            lastWeek: 5,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 12,
+            lastWeek: 2,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 25,
-          lastWeek: 5
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 12,
-          lastWeek: 2
-        }
       },
       {
         establishmentName: "شركة الريادة العالمية للتجارة والتسويق",
-        issued: {
-          sinceLaunch: 360,
-          lastWeek: 29
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 360,
+            lastWeek: 29,
+          },
+          canceledAjeer: {
+            sinceLaunch: 40,
+            lastWeek: 7,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 19,
+            lastWeek: 4,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 40,
-          lastWeek: 7
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 19,
-          lastWeek: 4
-        }
       },
       {
         establishmentName: "شركة الواحة النشطة للخدمات العمالية",
-        issued: {
-          sinceLaunch: 280,
-          lastWeek: 23
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 280,
+            lastWeek: 23,
+          },
+          canceledAjeer: {
+            sinceLaunch: 32,
+            lastWeek: 6,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 15,
+            lastWeek: 3,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 32,
-          lastWeek: 6
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 15,
-          lastWeek: 3
-        }
       },
       {
         establishmentName: "شركة الأفق الجديد للتجارة والتسويق",
-        issued: {
-          sinceLaunch: 470,
-          lastWeek: 38
+        reportTransactions: {
+          issuedVisas: {
+            sinceLaunch: 470,
+            lastWeek: 38,
+          },
+          canceledAjeer: {
+            sinceLaunch: 52,
+            lastWeek: 10,
+          },
+          canceledMhrsd: {
+            sinceLaunch: 23,
+            lastWeek: 5,
+          },
         },
-        cancelledAjeer: {
-          sinceLaunch: 52,
-          lastWeek: 10
-        },
-        cancelledMhrsd: {
-          sinceLaunch: 23,
-          lastWeek: 5
-        }
       },
     ],
   },
